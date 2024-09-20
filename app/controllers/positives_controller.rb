c180eae0852fff3ae12d4403755c2322f3294fb8
firecomenntdo <shikaneko6@gmail.com>
2024-09-20 15:51:48 +0900

    表示

--- app/controllers/positives_controller.rb
+++ app/controllers/positives_controller.rb
@@ -2,7 +2,7 @@ class PositivesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   def index
     @new_positive = Positive.new
-    @positive = Positive.where('extract(year from positives.created_at) 
+    @positive = current_user.positives.where('extract(year from positives.created_at) 
 = ? AND extract(month from positives.created_at) = ?', 
 Time.now.year, Time.now.month)
 
@@ -19,7 +19,9 @@ Time.now.year, Time.now.month)
       render :index, status: :unprocessable_entity
     end
   end
-
+  def show
+    
+  end
   private
   def positive_params
     params.require(:positive).permit(:emotion_lv_id,:positive_context).merge(user_id: current_user.id)
