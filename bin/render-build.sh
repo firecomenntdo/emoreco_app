59b3c5c1cd8423cf8d71e3969c8d197568fdf133
firecomenntdo <shikaneko6@gmail.com>
2024-09-28 00:31:19 +0900

    q

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,4 +9,5 @@ bundle exec rake db:migrate
 
 # Restore the MySQL database from backup
 # 環境変数を使用してpsqlコマンドを実行
+export PGPASSWORD=$DB_PASSWORD
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f backup.sql
