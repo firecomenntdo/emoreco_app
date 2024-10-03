dce19d4cd6eed20aea9a798591be6645c67e632d
firecomenntdo <shikaneko6@gmail.com>
2024-10-03 22:23:18 +0900

    削除機能

--- app/controllers/positives_controller.rb
+++ app/controllers/positives_controller.rb
@@ -20,9 +20,22 @@ class PositivesController < ApplicationController
       render :index, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    positive = Positive.find(params[:id])
+    positive.positive_tag_relations.destroy_all
+    positive.destroy
+    redirect_to positive_path
+  end
   def show
     @positives = current_user.positives.all
     @tag_list = Tag.all
+    @positive = current_user.positives.where('extract(year from positives.created_at) 
+    = ? AND extract(month from positives.created_at) = ?', 
+    Time.now.year, Time.now.month)
+
+    @positive_ratio = @positive.joins("LEFT JOIN emotion_lvs ON emotion_lvs.id = positives.emotion_lv_id")
+    .group("emotion_lvs.level").count
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'positives#index'
-  resources :positives, :negatives, only: [:index, :create, :show] do
+  resources :positives, :negatives, only: [:index, :create, :show, :destroy] do
     collection do
       get 'search'
     end
